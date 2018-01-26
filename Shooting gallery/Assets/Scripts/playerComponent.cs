d4838b81a9bc7907d94aca32ccca79b8dbe5b0c5
Matthew Farias <ezioblade64@gmail.com>
2018-01-25 23:18:35 -0500

    Init Commit

---
+++ Shooting gallery/Assets/Scripts/playerComponent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerComponent : MonoBehaviour {
+	public float movespeed;
+	// Use this for initialization
+	void Start () {
+		movespeed = 5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (movespeed * Input.GetAxis ("Horizontal") * Time.deltaTime, 0f, movespeed * Input.GetAxis ("Vertical") * Time.deltaTime);
+	}
+}
---
+++ Shooting gallery/Assets/Scripts/projectileShooter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectileShooter : MonoBehaviour {
+	public GameObject bullet;
+	public GameObject bulletHole;
+	public float delayTime=8;
+	public AudioSource sound;
+
+	private float counter=0;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetMouseButtonDown(0) && counter>delayTime){
+			Instantiate(bullet,transform.position,transform.rotation);
+			sound.Play();
+
+			RaycastHit hit;
+			Ray ray = new Ray(transform.position,transform.forward);
+			if(Physics.Raycast(ray, out hit, 100f)){
+				Instantiate(bulletHole,hit.point,Quaternion.FromToRotation(Vector3.up, hit.normal));
+			}
+		}
+		counter += Time.deltaTime;
+
+
+	}
+}
